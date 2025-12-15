10cce702b17a8b4d9c68ae21d8d628ab22463a28
Claus Holbech <holbech@arista.com>
2025-12-15 17:33:44 +0100

    Bump: Pre-release v0.0.1-dev4

--- pyavd_utils/__init__.py
+++ pyavd_utils/__init__.py
@@ -7,6 +7,6 @@ from . import passwords, validation
 __author__ = "Arista Networks"
 __copyright__ = "Copyright 2025 Arista Networks"
 __license__ = "Apache 2.0"
-__version__ = "0.0.1.dev3"
+__version__ = "0.0.1.dev4"
 
 __all__ = ["passwords", "validation"]
