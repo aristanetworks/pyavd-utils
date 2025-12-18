0dcca6da6a50151e66dadd6f6868f69d30454530
gmuloc <gmulocher@arista.com>
2025-12-18 17:40:01 +0100

    Feat(passwords): Add CBC TripleDes support for Type7 passwords

--- rust/passwords/src/lib.rs
+++ rust/passwords/src/lib.rs
@@ -3,8 +3,13 @@
 // that can be found in the LICENSE file.
 #![deny(unused_crate_dependencies)]
 
+// TODO: the crate is seen as unused, need to get why
+use cipher as _;
+
+mod cbc;
 mod errors;
 mod sha512crypt;
 
-pub use sha512crypt::sha512_crypt;
+pub use cbc::{cbc_check_password, cbc_decrypt, cbc_encrypt};
 pub use errors::{InvalidSaltError, Sha512CryptError};
+pub use sha512crypt::sha512_crypt;
