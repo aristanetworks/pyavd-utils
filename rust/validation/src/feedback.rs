cadf155670ba4512f2b120ce3e4cf7b15651424a
Claus Holbech <holbech@arista.com>
2026-03-23 12:09:04 +0100

    remove unused violation enum variant

--- rust/validation/src/feedback.rs
+++ rust/validation/src/feedback.rs
@@ -173,9 +173,6 @@ pub enum Violation {
     /// The dictionary key is required, but was not set.
     #[display("Missing the required key '{key}'.")]
     MissingRequiredKey { key: String },
-    /// The given schema name was not found in the schema store.
-    #[display("Invalid Schema name '{schema}'.")]
-    InvalidSchema { schema: String },
     /// The value is not of the expected type.
     #[display("Invalid type '{found}'. Expected '{expected}'.")]
     InvalidType { expected: Type, found: Type },
